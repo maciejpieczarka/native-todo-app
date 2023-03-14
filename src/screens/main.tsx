1ab28db415396931c6a258b26279fa1f467a4302
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-03-14 15:01:21 +0100

    Initial commit

---
+++ src/screens/main.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text, Box, Center } from 'native-base';
+
+export default function MainScreen() {
+  return <Center _dark={{ bg: 'blueGray.900' }}></Center>;
+}
